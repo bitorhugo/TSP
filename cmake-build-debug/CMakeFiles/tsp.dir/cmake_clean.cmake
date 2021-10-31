5af6f25242da136795c2e595ac4375fd8a5dae0f
Vitor <vitorhugosantos144@gmail.com>
2021-10-31 15:29:58 +0000

    first commit of tsp

---
+++ cmake-build-debug/CMakeFiles/tsp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tsp.dir/main.c.o"
+  "tsp"
+  "tsp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tsp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
