dd24ce9c2251625510f3f15525ac163be31eabfb
Vitor <vitorhugosantos144@gmail.com>
2021-11-01 16:32:56 +0000

    Refactored function create_new_client

--- client.c
+++ client.c
@@ -6,10 +6,15 @@
 
 
 Client *create_new_client(uint32_t id) {
-    Client *new_client = malloc(sizeof(new_client));
-    new_client->next_in_line = NULL;
-    new_client->user_id = id;
-    printf("New client at %p\n", new_client);
+    Client *new_client = malloc(sizeof(Client));
+    if (new_client != NULL) { // Checks if program was able to allocate memory successfully
+        new_client->next_in_line = NULL;
+        new_client->user_id = id;
+        printf("New client at %p\n", new_client);
+    }
+    else {
+        printf("Not able to allocate memory");
+    }
     return new_client;
 }
 
--- main.c
+++ main.c
@@ -5,12 +5,16 @@
 
 int main() {
 
-    sClient *first = create_new_client(1);
-    sClient *second = create_new_client(2);
-//    first->next_in_line = second;
+    Client *first = create_new_client(1);
+    Client *second = create_new_client(2);
+    Client *third = create_new_client(3);
+
+    first->next_in_line = second;
+    second->next_in_line = third;
 
     print_client(first);
     print_client(second);
+    print_client(third);
 
     return 0;
 }
